072d2931cf004c8d777fecc6171bfa725f1065f7
Jose Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2019-05-10 11:43:45 -0500

    env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import 'dotenv/config';
-
 const port = process.env.PORT || 8080
 
 async function bootstrap() {
