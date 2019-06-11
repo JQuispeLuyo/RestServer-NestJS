271528c9744de734bbe6233e94d762612ffadd7d
Jhordy Miguel Caceres Guerra <jhordy.caceres@vallegrande.edu.pe>
2019-06-11 12:49:36 -0500

    Prueba de despliegue automatico con github

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'CropRest by Jose Luis Quispe Luyo';
+    return ':D';
   }
 }
