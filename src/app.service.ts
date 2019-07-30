785ba6936bfeff44f5416f7abeba0f7fd00b718d
Jose Luis Quispe Luyo <jose.quispe@vallegrande.edu.pe>
2019-07-29 22:19:38 -0500

    Deploy in Azure DevOps and Heroku

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Jose Quispe :D...';
+    return 'Jose Quispe :D... deploy CropApi';
   }
 }
